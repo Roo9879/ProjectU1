9e414c205ee72b545c7629a0bfa64f3952f5c286
Roo9879 <rarrosie77@gmail.com>
2025-10-09 15:03:29 +0100

    add \n to reduce repeated code. Minor cleanups

--- src/src/FinCoreCLI.java
+++ src/src/FinCoreCLI.java
@@ -4,14 +4,12 @@ public class FinCoreCLI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int currentBalance = 1000;
+        double currentBalance = 1000;
         String accountHolderName = "Jane Doe";
 
-        System.out.println(" ");
-        System.out.println("Welcome to FinCore CLI Banking!");
-        System.out.println(" ");
+        System.out.println("\nWelcome to FinCore CLI Banking!");
 
-        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("\nAccount Holder: " + accountHolderName);
         //pre-determined for now but assuming we'll add in options to input name so it can fetch correct account balance etc
 
         System.out.println("Initial Balance: £" + currentBalance);
@@ -19,8 +17,7 @@ public class FinCoreCLI {
         String option;
 
         do {
-            System.out.println(" ");
-            System.out.println("=== FinCore CLI Banking Menu ===");
+            System.out.println("\n=== FinCore CLI Banking Menu ===");
             System.out.println("1. Deposit");
             System.out.println("2. Withdraw");
             System.out.println("3. Check Balance");
@@ -30,7 +27,7 @@ public class FinCoreCLI {
 
             switch (option) {
                 case "1": //deposit
-                    currentBalance = makeDeposit(scanner, currentBalance);
+                    currentBalance = makeDeposit(scanner, currentBalance); //updates currentBalance value with returned version from method
                     break;
                 case "2": //withdraw
                     currentBalance = makeWithdrawal(scanner, currentBalance);
@@ -39,34 +36,31 @@ public class FinCoreCLI {
                     checkBalance(currentBalance, accountHolderName);
                     break;
                 case "4": //exit
-                    System.out.println(" ");
-                    System.out.println("Thank you for using FinCore CLI Banking!");
+                    System.out.println("\nThank you for using FinCore CLI Banking!");
                     break;
                 default: //invalid choices
                     System.out.println("Invalid choice. Please try again");
-                    break; // want it to get them to try again
+                    break;
             }
         } while (!option.equals("4"));
     }
 
-    public static int makeDeposit(Scanner scanner, int currentBalance) {
+    public static double makeDeposit(Scanner scanner, double currentBalance) {
         boolean valid = false;
-        int depositAmount = 0;
+        double depositAmount = 0;
         while (!valid) {
-            System.out.println(" ");
-            System.out.print("Enter your amount to deposit (or 'x' to cancel): £");
+            System.out.print("\nEnter your amount to deposit (or 'x' to cancel): £");
             String input = scanner.nextLine();
 
             // check for cancel option
             if (input.equalsIgnoreCase("x")) {
-                System.out.println(" ");
-                System.out.println("Deposit cancelled, returning to main menu");
+                System.out.println("\nDeposit cancelled, returning to main menu");
                 return currentBalance;
             }
             // parsing input (String) to int so can add onto balance
             // validation so must be a positive integer and a number, not a string, char or symbol etc
             try{
-                depositAmount = Integer.parseInt(input);
+                depositAmount = Double.parseDouble(input);
                 if(depositAmount > 0){
                     valid = true;
                 } else {
@@ -78,40 +72,37 @@ public class FinCoreCLI {
         }
         // adding deposit amount to balance and outputting to user
         currentBalance += depositAmount;
-        System.out.println(" ");
-        System.out.println("Deposit successful!");
+        System.out.println("\nDeposit successful!");
         System.out.println("Amount deposited: £" + depositAmount);
         System.out.println("Your new balance is: £" + currentBalance);
         return currentBalance;
     }
 
-    public static int makeWithdrawal(Scanner scanner, int currentBalance) {
+    public static double makeWithdrawal(Scanner scanner, double currentBalance) {
         while (true) {
-            System.out.println(" ");
-            System.out.print("Enter your amount to withdraw (or 'x' to cancel): £");
+            System.out.print("\nEnter your amount to withdraw (or 'x' to cancel): £");
             String input = scanner.nextLine();
 
             //check for cancel option
             if (input.equalsIgnoreCase("x")) {
-                System.out.println(" ");
-                System.out.println("Withdrawal cancelled, returning to main menu");
+                System.out.println("\nWithdrawal cancelled, returning to main menu");
                 return currentBalance;
             }
             // parsing string and validation steps
             try{
-                int withdrawalAmount = Integer.parseInt(input);
+                double withdrawalAmount = Double.parseDouble(input);
                 if(withdrawalAmount <=0){
                     System.out.println("Please enter a positive integer");
                 } else if(withdrawalAmount > currentBalance){
                     // withdrawal amount must be smaller than current balance
                     // otherwise would go into -ves
-                    System.out.println("Insufficient funds.");
+                    System.out.println("Error: Insufficient funds");
+                    System.out.println("Attempted withdrawal: £" + withdrawalAmount);
                     System.out.println("Your current balance is £" + currentBalance);
                 } else {
                     // actual withdrawal calculation and output to user
                     currentBalance -= withdrawalAmount;
-                    System.out.println(" ");
-                    System.out.println("Withdrawal successful!");
+                    System.out.println("\nWithdrawal successful!");
                     System.out.println("Amount withdrawn: £" + withdrawalAmount);
                     System.out.println("Your new balance is: £" + currentBalance);
                     return currentBalance;
@@ -123,9 +114,8 @@ public class FinCoreCLI {
         }
     }
 
-    public static void checkBalance(int currentBalance, String accountHolderName) {
-        System.out.println(" ");
-        System.out.println("=== Account Balance ===");
+    public static void checkBalance(double currentBalance, String accountHolderName) {
+        System.out.println("\n=== Account Balance ===");
         System.out.println("Account Holder: " + accountHolderName);
         System.out.println("Current Balance: £" + currentBalance);
     }
